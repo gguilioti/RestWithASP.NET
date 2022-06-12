aa8d8f0f17c96d9fe61ded95a7fa5ced1de5cf42
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-06-11 22:50:10 -0300

    Verbs HTTP implemented

---
+++ RestASPNET/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
