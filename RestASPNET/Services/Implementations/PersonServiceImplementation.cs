1c7d128eddc5ce50c5b442cca5e6ccddb12711d5
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-06-13 20:22:31 -0300

    crud implemented

--- RestASPNET/Services/Implementations/PersonServiceImplementation.cs
+++ RestASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -18,12 +18,55 @@ namespace RestASPNET.Services.Implementations
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
             return person;
         }
 
         public void Delete(long id)
         {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
             
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
         }
 
         public List<Person> FindAll()
@@ -33,19 +76,12 @@ namespace RestASPNET.Services.Implementations
 
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = 1,
-                FirstName = "Giovanni",
-                LastName = "Guilioti",
-                Adress = "Maringá",
-                Gender = "Male"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
-        public Person Update(Person person)
+        private bool Exists(long id)
         {
-            return person;
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
