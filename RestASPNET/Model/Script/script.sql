fb3019a079908da9ba8afde2869fe6a0133344d7
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-06-13 19:40:56 -0300

    added a database

---
+++ RestASPNET/Model/Script/script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS person (
+id INTEGER PRIMARY KEY,
+adress varchar(100) NOT NULL,
+first_name varchar(80) NOT NULL,
+last_name varchar(80) NOT NULL,
+gender varchar(8) NOT NULL
+);
