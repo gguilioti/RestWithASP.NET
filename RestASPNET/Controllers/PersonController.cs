ec875121cee70bf8666edd24d023f1a94e43b150
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-06-18 19:40:34 -0300

    api versioning

--- RestASPNET/Controllers/PersonController.cs
+++ RestASPNET/Controllers/PersonController.cs
@@ -9,8 +9,9 @@ using System.Threading.Tasks;
 
 namespace RestASPNET.Controllers
 {
+    [ApiVersion("1.0")]
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/v{version:apiVersion}")]
     public class PersonController : ControllerBase
     {
 
